6669721dfa01c3be7dd35a58ba2344c81558fea6
Anom Maharjan <anommaharjan@prixatech.com>
2022-02-02 21:12:48 +0545

    homepage added

--- lib/app/modules/home_page/controllers/home_page_controller.dart
+++ lib/app/modules/home_page/controllers/home_page_controller.dart
@@ -1,20 +1,147 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+class Model {
+  IconData icon;
+  String name;
+  VoidCallback onTap;
+
+  Model(this.name, this.icon, this.onTap);
+}
+
+
 class HomePageController extends GetxController {
-  //TODO: Implement HomePageController
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+
+ late var pressed = false;
+  late var click = false;
+  final List itemDetails = [
+    Model("Buy Policy", Icons.person, () {
+      
+    }),
+    Model("Premium Calculator", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+      // Get.to(PremiumCalculatorView());
+    }),
+    Model("Pay Premium", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+    Model("Claim Intimation", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+    Model("Cheque Claim", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+    Model("Check Policy", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+  ];
+
+  final List calculatorDetails = [
+    Model("Premium Calculator", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+      
+    }),
+    Model("Pay Premium", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+    Model("Claim Intimation", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+    Model("Cheque Claim", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+    Model("Check Policy", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+    Model("Check Policy", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+    Model("Check Policy", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+    Model("Check Policy", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+    Model("Premium Calculator", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+      
+    }),
+    Model("Premium Calculator", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+      
+    }),
+    Model("Pay Premium", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+    Model("Claim Intimation", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+    Model("Premium Calculator", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+      
+    }),
+    Model("Pay Premium", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+    Model("Claim Intimation", Icons.person, () {
+      Container(
+        color: Colors.red,
+      );
+    }),
+  ];
+
+  final List images = [
+    'assets/images/img1.jpg',
+    'assets/images/img1.jpg',
+    'assets/images/img1.jpg',
+  ];
+
+  colorUpdate() {
+    pressed = !pressed;
+    update();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  clickUpdate() {
+    click = !click;
+    update();
   }
-
-  @override
-  void onClose() {}
-  void increment() => count.value++;
 }
